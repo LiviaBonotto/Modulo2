e05da06b471f613e7e368bb3e3a16ed9a4c0eded
Lívia Bonotto <liviabonotto@gmail.com>
2022-05-23 00:19:55 -0300

    Autoestudo:  Exercício de personalização da Etapa 3

--- 03_AUT_EST_ENTREGA/Semana 4/exercicio_personalização_etapa3/frontend/script.js
+++ 03_AUT_EST_ENTREGA/Semana 4/exercicio_personalização_etapa3/frontend/script.js
@@ -1,2 +1,46 @@
-/* .js files add interaction to your website */
+var inteli = document.getElementById("inteli")
+var cefet = document.getElementById("cefet")
+var curso = document.getElementById("curso")
+var cubo = document.getElementById("cubo")
+var ifrj = document.getElementById("ifrj")
+var formacao = document.getElementById("formacao")
+var experiencias = document.getElementById("experiencias")
+var geral = document.getElementById("caixa")
+var geral2 = document.getElementById("caixa2")
+var gwc = document.getElementById("gwc")
+var hc = document.getElementById("hc")
+var boot = document.getElementById("boot")
+var ibm = document.getElementById("ibm")
+var zion = document.getElementById("zion")
+
+
+
+
+
+
+function Inicio() {
+    geral.removeChild(formacao);
+    geral2.removeChild(experiencias)
+}
+
+function Text() {
+    geral.appendChild(inteli)
+    geral.appendChild(cefet)
+    geral.appendChild(curso)
+    geral.appendChild(cubo)
+    geral.appendChild(ifrj)
+
+}
+
+function Text2() {
+    geral2.appendChild(gwc)
+    geral2.appendChild(hc)
+    geral2.appendChild(boot)
+    geral2.appendChild(ibm)
+    geral2.appendChild(zion)
+
+
+
+
+}
 
