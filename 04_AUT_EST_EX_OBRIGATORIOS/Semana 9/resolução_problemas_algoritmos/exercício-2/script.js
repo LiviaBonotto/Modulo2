40d3122c5c39c3e51d43cc00618ab4de9af6d7d4
Lívia Bonotto <liviabonotto@gmail.com>
2022-06-21 16:55:14 -0300

    Semana 9 - exercício 2

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/resolução_problemas_algoritmos/exercício-2/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 9/resolução_problemas_algoritmos/exercício-2/script.js
@@ -1,11 +1,16 @@
-function trocaInputs (input1, input2) {
-    const in1 = document.getElementById("input1");
-    const in2 = document.getElementById("input2");
-    const in1Value = in1.value;
-    in1.value = in2.value;
-    in2.value = in1Value;
-}
+function soma() {
+    let numero = document.getElementById("numeroInput").value;
 
-function trocaPosicao () {
-    trocaInputs("input1, input2");
+    if (numero > 0) {
+        var soma = 0;
+        while (numero > 0){
+            soma = soma + numero % 10;
+            numero = parseInt(numero / 10);
+        }
+        alert("A soma dos dígitos é igual a: " + soma);
+    }
+
+    else {
+        alert("Valor inválido!");
+    }
 }
