5c5831bb284a2df3675c05d00960d84a6d13b6f1
Lívia Bonotto <liviabonotto@gmail.com>
2022-06-21 10:13:45 -0300

    Semana 7 - resolução ex - exercício 1

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/resolução_problemas_algoritmos/exercício-1/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/resolução_problemas_algoritmos/exercício-1/script.js
@@ -1,14 +1,22 @@
 function adicionar(numeroInput) {
-    const input = document.getElementById(numeroInput);
-    let valor = parseInt(input.valor);
-    const novoValor = valor + 1;
-    input.valor = novoValor;
+    const input = document.getElementById("numeroInput");
+    let value = parseInt(input.value);
+    const novoValor = value + 1;
+    input.value = novoValor;
 }
 
 function remover(){
-
+    const input = document.getElementById("numeroInput");
+    let value = parseInt(input.value);
+    const novoValor = value - 1;
+    input.value = novoValor;
 }
 
 function enviaSoma() {
     adicionar("numeroInput");
 }
+
+function enviaSubtracao() {
+    remover("numeroInput");
+}
+
